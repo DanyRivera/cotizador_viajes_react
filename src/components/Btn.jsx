712ded19baf2fd4f5688f6248fc6b72f20e1318e
danyy <dany@gmail.com>
2022-01-18 19:00:32 -0600

    v1.4 (Arreglando detalles del diseño responsive)

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -24,7 +24,7 @@ const Btn = ({ texto }) => {
     }
 
     return (
-        <div className="flex justify-end mt-10">
+        <div className={`flex ${texto == 'Resetear App' ? 'justify-center' : 'justify-end'} mt-10`}>
             <button
                 className="block bg-[#10c210] text-xl rounded text-white py-3 px-10 transition-all ease-in hover:bg-[#0aa50a]"
                 onClick={e => handleClick()}
